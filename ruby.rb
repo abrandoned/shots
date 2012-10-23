54ff75f0fd92c92e0264b7f0059f1ae141077a3c
Brandon <brandon@myjibe.com>
2012-10-23 16:25:28 -0400

    array block

--- ruby.rb
+++ ruby.rb
@@ -1,3 +1,16 @@
+array = (1..10).to_a
+
+for item in array do 
+  puts item
+end
+
+array.each do |item|
+  puts item
+end
+
+
+
+
 hash = { :this => "that", "thing" => "stuff", 2 => 3, /hello/ => /goodbye/ }
 
 array = [true, false, "this", "that", "other", [2, 3, 4], hash]
