27a68c255810a0c1dd68c00d9b01c9d90bf88805
Brandon <brandon@myjibe.com>
2012-10-23 16:14:26 -0400

    add arrays

---
+++ ruby.rb
@@ -0,0 +1,10 @@
+hash = { :this => "that", "thing" => "stuff", 2 => 3, /hello/ => /goodbye/ }
+
+array = [true, false, "this", "that", "other", [2, 3, 4], hash]
+
+regular_expression = /hello/
+
+symbol = :symbol
+
+
+
